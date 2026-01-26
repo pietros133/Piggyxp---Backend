4a931682754412fd573d918de3e45ae71f6ed9ed
eyanprogh <ryannaosei329@gmail.com>
2026-01-26 12:29:50 -0300

    Update User protegido

--- src/controllers/updateUserController.js
+++ src/controllers/updateUserController.js
@@ -1,8 +1,28 @@
 import { updateUserService } from "../services/updateUserService.js";
+import jwt from "jsonwebtoken";
 
 export async function updateUserController(req, res) {
 
     try {
+    
+    // Extrai token do header Authorization
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: "Token não fornecido" });
+    }
+    
+    const token = authHeader.split(" ")[1]; // Bearer <TOKEN>, o do login
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    const loggedUserId = Number(decoded.userId);
+    const userIdToUpdate = Number(req.params.id);
+    
+    // só pode deletar a si mesmo
+    if (loggedUserId !== userIdToUpdate) {
+      return res.status(403).json({ message: "Você não pode atualizar este usuário" });
+    }
+    
+
     const { id } = req.params;
     const { name, email } = req.body;
 
