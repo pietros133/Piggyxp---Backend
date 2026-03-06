19a52733a2e5741e3c91b75289f7ab765becaf05
eyanprogh <ryannaosei329@gmail.com>
2026-03-06 18:08:15 -0300

    getPhases alterado

--- src/services/getPhasesService.js
+++ src/services/getPhasesService.js
@@ -1,13 +1,13 @@
-import phasesData from "../content/units/unit1/phases.json" with { type: "json" };
+import { MongoDataSource } from "../mongo/database/mdbconnect.js";
+import { Phases } from "../mongo/models/Phases.js";
 
 export async function getPhasesService(userDifficulty, userOrder) {
-  const phases = phasesData.phases;
+  const phasesRepository = MongoDataSource.getMongoRepository(Phases);
 
-  const phase = phases.find(
-    (p) =>
-      p.dificulty === Number(userDifficulty) &&
-      p.order === Number(userOrder)
-  )
+  const phase = await phasesRepository.findOneBy({
+      dificulty: Number(userDifficulty),
+      order: Number(userOrder)
+  })
 
   if (!phase) {
     throw new Error("Fase não encontrada");
