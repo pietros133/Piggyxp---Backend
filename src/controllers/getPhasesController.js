bdfd4d9a8c453887beba96db93738aeddb23ba9c
eyanprogh <ryannaosei329@gmail.com>
2026-03-17 21:41:54 -0300

    getPhases 2.0

--- src/controllers/getPhasesController.js
+++ src/controllers/getPhasesController.js
@@ -2,15 +2,15 @@ import { getPhasesService } from "../services/getPhasesService.js"
 
 export async function getPhasesController(req, res) {
   try {
-    const { difficulty, order } = req.query
+    const { difficulty, order, unit } = req.query
 
-    if (!difficulty || !order) {
+    if (!difficulty || !order || !unit) {
       return res.status(400).json({
-        message: "difficulty e order são obrigatórios"
+        message: "unit, difficulty e order são obrigatórios"
       })
     }
 
-    const phase = await getPhasesService(difficulty, order)
+    const phase = await getPhasesService(difficulty, order, unit)
 
     return res.status(200).json(phase)
 
--- src/services/getPhasesService.js
+++ src/services/getPhasesService.js
@@ -1,12 +1,13 @@
 import { MongoDataSource } from "../mongo/database/mdbconnect.js";
 import { Phases } from "../mongo/models/Phases.js";
 
-export async function getPhasesService(userDifficulty, userOrder) {
+export async function getPhasesService(userDifficulty, userOrder, userUnit) {
   const phasesRepository = MongoDataSource.getMongoRepository(Phases);
 
   const phase = await phasesRepository.findOneBy({
       dificulty: Number(userDifficulty),
-      order: Number(userOrder)
+      order: Number(userOrder),
+      unit: Number(userUnit)
   })
 
   if (!phase) {
